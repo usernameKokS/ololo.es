5bf2901f2a7f1f2d5513e3cf8c076301d6910f65
usernameKokS <86251837+usernameKokS@users.noreply.github.com>
2022-02-23 13:09:20 +0300

    Тест корректной загрузки фотографий 4

--- app/Http/Controllers/AvatarController.php
+++ app/Http/Controllers/AvatarController.php
@@ -60,7 +60,7 @@ class AvatarController extends Controller
         $imageName = Str::random(6) . 'de' . time().'.'.$request->avatar->extension();  
             
 		
-        $r = Storage::putFileAs('public/avatar/', new File($request->avatar), $imageName);
+        $r = Storage::putFileAs('public/avatar', new File($request->avatar), $imageName);
 		
         try
         {
@@ -97,7 +97,7 @@ class AvatarController extends Controller
 
     function convertImage($originalImage)
     {
-		$rootDir = $_SERVER['DOCUMENT_ROOT'] . 'storage/app/public/avatar/';
+		$rootDir = $_SERVER['DOCUMENT_ROOT'] . '/storage/app/public/avatar/';
 		
         // jpg, png, gif or bmp?
         $exploded = explode('.', $originalImage);
