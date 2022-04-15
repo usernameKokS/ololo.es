b80b2d438889be5c025a5a22284940842dbd490c
usernameKokS <86251837+usernameKokS@users.noreply.github.com>
2022-04-15 15:29:42 +0300

    Yandex metrika delete

--- resources/views/landing/index_en.blade.php
+++ resources/views/landing/index_en.blade.php
@@ -34,28 +34,7 @@
 
 	  gtag('config', 'UA-179635663-1');
 	</script>
-	<!-- Yandex.Metrika counter -->
-	<script type="text/javascript" >
-	   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-	   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-	   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-	   ym(67951603, "init", {
-			clickmap:true,
-			trackLinks:true,
-			accurateTrackBounce:true,
-			webvisor:true,
-			ecommerce:"dataLayer"
-	   });
-	</script>
-	<noscript><div><img src="https://mc.yandex.ru/watch/67951603" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-	<!-- /Yandex.Metrika counter -->
-	<script>
-        (function(w,d,u){
-                var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/60000|0);
-                var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);
-        })(window,document,'https://cdn-ru.bitrix24.ru/b16684324/crm/site_button/loader_1_tnupw5.js');
-</script>
 </head>
 
 <body class="custom_class">
